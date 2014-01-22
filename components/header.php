244b9ef39f5edf5fe6dd62eb49d12f6adedbf718
conradhappeliv <conrad.h.appel.iv@gmail.com>
2014-01-21 20:22:24 -0600

    Template System Almost Functional
    Broken as of now but soon....

--- components/header.php
+++ components/header.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $p->title; ?></title>
-	<meta name="description" content="<?php echo $p->description; ?>">
+	<title><?php echo $this->title; ?></title>
+	<meta name="description" content="<?php echo $this->description; ?>">
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
--- php/page.php
+++ php/page.php
@@ -6,18 +6,20 @@
 		protected $templateorder;
 		
 		public function __construct($i) {
-			$title = $i['title'];
-			$description = $i['description'];
-			$template = $i['template'];
-			$templateorder = explode(",",file_get_contents("./templates/".$template));
-			while($n = next($templateorder)) {
+			$this->title = $i['title'];
+			$this->description = $i['description'];
+			$this->template = $i['template'];
+			$this->templateorder = explode(",",file_get_contents("./templates/".$this->template));
+			foreach($this->templateorder as $n) {
 				if($n == "content") break;
-				else include_once("../components/".$n.".php");
+				else include_once("./components/".$n.".php");
 			}
 		}
 		
 		function endpage() {
-		
+			$initialkey = array_search("content",$this->templateorder);
+			$arraylength = count($this->templateorder);
+			//for($i = $initialkey; $i++; $i<$arraylength) include_once("./components/".$i.".php");
 		}
 	}
 ?>
