b0535a8d87e0251683105ae7a1fc10ef9a0b4b56
Austin Wells <acwells@smu.edu>
2014-02-01 18:53:58 -0600

    added back button to project page

--- project.php
+++ project.php
@@ -14,18 +14,20 @@
 	<div class="col-lg-10">
 		<div class="panel panel-primary">
 		  <!-- Default panel contents -->
-		  <div class="panel-heading" id="ProjectTitle">Project Title</div>
+		  <div class="panel-heading" id="projectTitle">Project Title
+			<button type="button" class="close" aria-hidden="true" >&times;</button>
+		  </div>
 
 
 		  <!-- List group -->
 		  <ul class="list-group">
-		    <li class="list-group-item" id="SubmitDate">Submitted: 11/12/13</li>
-		    <li class="list-group-item" id="ContactNum">Contact # 123-456-7890</li>
-		    <li class="list-group-item" id="Sponsor">Sponsor: Microsoft</li>
-		    <li class="list-group-item" id="ProjectDescription">Project Description: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, ducimus, quam odio eligendi totam suscipit optio reiciendis quas excepturi facere quia nemo. Officiis sit adipisci quasi voluptates excepturi. Sed, cumque.</li>
-		   	<li class="list-group-item" id="UserNeeds">Primary User Needs: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, labore, in quisquam dolores blanditiis facilis culpa reprehenderit iste deserunt numquam quod nemo cumque excepturi consectetur aut repudiandae nam sapiente officiis!</li>
-		    <li class="list-group-item" id="AdditionalResources">Additional Resources Provided: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, doloribus excepturi repudiandae consectetur quam obcaecati quibusdam tempora nam ex quaerat minima dolor aperiam temporibus iure tempore reiciendis quae sit neque!</li>
-		    <li class="list-group-item" id="Links">Links:</li>
+		    <li class="list-group-item" id="submitDate">Submitted: 11/12/13</li>
+		    <li class="list-group-item" id="contactNum">Contact # 123-456-7890</li>
+		    <li class="list-group-item" id="sponsor">Sponsor: Microsoft</li>
+		    <li class="list-group-item" id="projectDescription">Project Description: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, ducimus, quam odio eligendi totam suscipit optio reiciendis quas excepturi facere quia nemo. Officiis sit adipisci quasi voluptates excepturi. Sed, cumque.</li>
+		   	<li class="list-group-item" id="userNeeds">Primary User Needs: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, labore, in quisquam dolores blanditiis facilis culpa reprehenderit iste deserunt numquam quod nemo cumque excepturi consectetur aut repudiandae nam sapiente officiis!</li>
+		    <li class="list-group-item" id="additionalResources">Additional Resources Provided: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, doloribus excepturi repudiandae consectetur quam obcaecati quibusdam tempora nam ex quaerat minima dolor aperiam temporibus iure tempore reiciendis quae sit neque!</li>
+		    <li class="list-group-item" id="radditionalFiles">Links:</li>
 		    <li class="list-group-item">
 				<button type="button" class="btn btn-success">Accept</button>
 				<button type="button" class="btn btn-danger">Decline</button>
