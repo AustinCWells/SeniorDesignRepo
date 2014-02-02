af2cfec21c25a32ef2e41f2f7dcf7546b75fa54f
Austin Wells <acwells@smu.edu>
2014-02-01 20:25:37 -0600

    fixed missing container for grid flow

--- project.php
+++ project.php
@@ -6,35 +6,36 @@
 	~ Login: yes
 	%%%
 -->
+<div class="container"> 
+	<div class='row'>
+		<br>
+		<br>
+		<div class="col-lg-1"> </div>
+		<div class="col-lg-10">
+			<div class="panel panel-primary">
+			  <!-- Default panel contents -->
+			  <div class="panel-heading" id="projectTitle">Project Title
+				<button type="button" class="close" aria-hidden="true" >&times;</button>
+			  </div>
 
-<div class='row'>
-	<br>
-	<br>
-	<div class="col-lg-1"> </div>
-	<div class="col-lg-10">
-		<div class="panel panel-primary">
-		  <!-- Default panel contents -->
-		  <div class="panel-heading" id="projectTitle">Project Title
-			<button type="button" class="close" aria-hidden="true" >&times;</button>
-		  </div>
 
-
-		  <!-- List group -->
-		  <ul class="list-group">
-		    <li class="list-group-item" id="submitDate">Submitted: 11/12/13</li>
-		    <li class="list-group-item" id="contactNum">Contact # 123-456-7890</li>
-		    <li class="list-group-item" id="sponsor">Sponsor: Microsoft</li>
-		    <li class="list-group-item" id="projectDescription">Project Description: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, ducimus, quam odio eligendi totam suscipit optio reiciendis quas excepturi facere quia nemo. Officiis sit adipisci quasi voluptates excepturi. Sed, cumque.</li>
-		   	<li class="list-group-item" id="userNeeds">Primary User Needs: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, labore, in quisquam dolores blanditiis facilis culpa reprehenderit iste deserunt numquam quod nemo cumque excepturi consectetur aut repudiandae nam sapiente officiis!</li>
-		    <li class="list-group-item" id="additionalResources">Additional Resources Provided: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, doloribus excepturi repudiandae consectetur quam obcaecati quibusdam tempora nam ex quaerat minima dolor aperiam temporibus iure tempore reiciendis quae sit neque!</li>
-		    <li class="list-group-item" id="radditionalFiles">Links:</li>
-		    <li class="list-group-item">
-				<button type="button" class="btn btn-success">Accept</button>
-				<button type="button" class="btn btn-danger">Decline</button>
-				<button type="button" class="btn btn-warning">Waitlist</button>
-		    </li>
-		  </ul>
+			  <!-- List group -->
+			  <ul class="list-group">
+			    <li class="list-group-item" id="submitDate">Submitted: 11/12/13</li>
+			    <li class="list-group-item" id="contactNum">Contact # 123-456-7890</li>
+			    <li class="list-group-item" id="sponsor">Sponsor: Microsoft</li>
+			    <li class="list-group-item" id="projectDescription">Project Description: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, ducimus, quam odio eligendi totam suscipit optio reiciendis quas excepturi facere quia nemo. Officiis sit adipisci quasi voluptates excepturi. Sed, cumque.</li>
+			   	<li class="list-group-item" id="userNeeds">Primary User Needs: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, labore, in quisquam dolores blanditiis facilis culpa reprehenderit iste deserunt numquam quod nemo cumque excepturi consectetur aut repudiandae nam sapiente officiis!</li>
+			    <li class="list-group-item" id="additionalResources">Additional Resources Provided: <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, doloribus excepturi repudiandae consectetur quam obcaecati quibusdam tempora nam ex quaerat minima dolor aperiam temporibus iure tempore reiciendis quae sit neque!</li>
+			    <li class="list-group-item" id="radditionalFiles">Links:</li>
+			    <li class="list-group-item">
+					<button type="button" class="btn btn-success">Accept</button>
+					<button type="button" class="btn btn-danger">Decline</button>
+					<button type="button" class="btn btn-warning">Waitlist</button>
+			    </li>
+			  </ul>
+			</div>
 		</div>
+		<div class="col-lg-1"> </div>
 	</div>
-	<div class="col-lg-1"> </div>
 </div>
--- projects.php
+++ projects.php
@@ -6,41 +6,43 @@
 	~ Login: yes
 	%%%
 -->
-<div class='row'>
-	<br>
-	<br>
-	<div class="col-lg-1"> </div>
-	<div class="col-lg-10">
+<div class='container'>
+	<div class='row'>
+		<br>
+		<br>
+		<div class="col-lg-1"> </div>
+		<div class="col-lg-10">
 
-<?php
-	new paginator();
+	<?php
+		new paginator();
 
-	//Get an array of the products on the current page
-	$projects = project::getProjects();
-	
-	//Display all the projects on this page
-	foreach ($projects as $project) {
-		?>
-
-	<div class="panel panel-default">
-		<div class="panel-heading">
-    		<h3 class="panel-title"><?= $project['title']; ?></h3>
-    		<a href="project.php?id=<?= $project['project_id']; ?>">
-				<strong><?= $project['project_id']; ?></strong>
-			</a>
-    	</div>
-    	<div class="panel-body">
-			
-			<p class="projectDescription"><b>Submission Date: </b> <?= $project['dt']; ?></p>
-			<p class="projectDescription"><b>Sponsor: </b> <?= $project['sponsorName']; ?></p>
-			<p><?= $project['description']; ?></p>
+		//Get an array of the products on the current page
+		$projects = project::getProjects();
 		
-		</div>
-	</div>
-		<?php
-	}
-?>
+		//Display all the projects on this page
+		foreach ($projects as $project) {
+			?>
 
+		<div class="panel panel-default">
+			<div class="panel-heading">
+	    		<h3 class="panel-title"><?= $project['title']; ?></h3>
+	    		<a href="project.php?id=<?= $project['project_id']; ?>">
+					<strong><?= $project['project_id']; ?></strong>
+				</a>
+	    	</div>
+	    	<div class="panel-body">
+				
+				<p class="projectDescription"><b>Submission Date: </b> <?= $project['dt']; ?></p>
+				<p class="projectDescription"><b>Sponsor: </b> <?= $project['sponsorName']; ?></p>
+				<p><?= $project['description']; ?></p>
+			
+			</div>
+		</div>
+			<?php
+		}
+	?>
+
+		</div>
+		<div class="col-lg-1"> </div>
 	</div>
-	<div class="col-lg-1"> </div>
 </div>
