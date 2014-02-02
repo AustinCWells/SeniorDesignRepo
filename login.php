4c5c6c1df1650be21dcbf65467d24213d80a9599
Austin Wells <acwells@smu.edu>
2014-02-01 21:15:27 -0600

    Fixed menu in responsive modes for cell phones and tablets

--- login.php
+++ login.php
@@ -106,9 +106,3 @@
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="css/bootstrap.min.js"></script>
